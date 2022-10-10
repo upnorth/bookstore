b7b7f4e3f089499347294b15891c65ff69dbb741
Karl Olofsson <biker.upnorth@gmail.com>
2022-10-10 14:55:01 +0200

    Simple test for bookstore.addBook()

--- tests/bookstore.test.ts
+++ tests/bookstore.test.ts
@@ -1,7 +1,19 @@
 import bookstore from "../src/bookstore";
+import { NewBook } from "../src/types/book";
 
 describe('bookstore', () => {
     test('should start with empty inventory', () => {
       expect(bookstore.getInventory()).toEqual([]);
+    });
+
+    test('should add book', () => {
+      const newBook: NewBook = { name: "Book", price: 100 }
+
+      const addedId = bookstore.addBook(newBook);
+
+      const currentInventory = bookstore.getInventory()
+      expect(typeof addedId).toBe("number")
+      expect(currentInventory.length).toBe(1);
+      expect(currentInventory[0]).toEqual(newBook);
     });
   });
